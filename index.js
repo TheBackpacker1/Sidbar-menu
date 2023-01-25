6f73c6f98d9e9db3d4b029221226021780583069
Mohamed louati <108157009+TheBackpacker1@users.noreply.github.com>
2023-01-25 04:05:41 +0100

    main

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+
+    $('i').click(function() {
+
+
+        $('ul').toggleClass('ul_show');
+        $('section').toggleClass('slide_image');
+    });
+    $('li').click(function() {
+
+
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
+
+});
